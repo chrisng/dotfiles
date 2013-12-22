86e4646f404aa3eadb7c737a76ccf7fd9f557e7d
Chris Ng <chaoselixir@gmailcom>
2013-12-21 22:51:43 -0800

    Added support for skip, backup

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+# ==============================================================================
+#
 # Script used to symlink dotfiles instead of copy
+#
+# TODO:
+#  Support copying if there is a dir
+#  example: 
+#     if [ -d $FILE ] ; then 
+#     for FILES in $FILE ; do
+#       CheckFileExists
+#       if PresentOptions
+#     done
+#
+# ==============================================================================
+
+DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+
 function link {
   FILE=$1
   ln -s `pwd`/$FILE ~/${FILE} 
@@ -8,12 +24,19 @@ function link {
 
 
 for FILE in `ls -1d .??* |grep -v "^.git$"` ; do
-  if [ -f ~/$FILE ] ; then
-    read -p "This will overwrite $FILE in your home directory. Are you sure? (y/n) " -n 1
+  if [[ -f $FILE ]] || [[ -d $FILE ]]; then
+    read -p "File $FILE in your home directory: Overwrite? Backup? Skip? Exit? (o/b/s/e) " -n 1
     echo 
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if [[ $REPLY =~ ^[Oo]$ ]]; then
       rm -f ~/$FILE
       link $FILE
+    elif [[ $REPLY =~ ^[Bb]$ ]]; then
+      mv ~/$FILE ~/${FILE}.${DATE}
+      link $FILE
+    elif [[ $REPLY =~ ^[Ss]$ ]]; then
+      echo "Skipping $FILE"
+    else
+      exit 15
     fi
   else
     link $FILE
