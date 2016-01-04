d32be25ba2b558b969c0368c1f28de85753e3fa6
Chris Ng <cng@enlitic.com>
2016-01-04 10:15:13 -0800

    Added aws cli completion for zsh

--- .zsh.after/load_exports.zsh
+++ .zsh.after/load_exports.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-FILES_TO_LOAD=(/usr/local/bin/virtualenvwrapper.sh ~/.exports)
+FILES_TO_LOAD=(/usr/local/bin/virtualenvwrapper.sh ~/.exports /usr/local/share/zsh/site-functions/_aws)
 
 for FILE in ${FILES_TO_LOAD[@]} ; do
   [ -f "${FILE}" ] && source $FILE
