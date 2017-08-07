efb8801f566908dcea687ba4f2b55c2c4dc2c27c
Chris Ng <cng4@apple.com>
2017-08-07 08:34:25 -0700

    Removed blue command highlighting

--- .zsh.after/load_keychain.zsh
+++ .zsh.after/load_keychain.zsh
@@ -2,5 +2,5 @@
 
 command -v keychain > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
-  eval `keychain --eval --agents ssh --eval id_ed25519 id_rsa`
+  #eval `keychain --eval --agents ssh --eval id_rsa` #id_ed25519
 fi
